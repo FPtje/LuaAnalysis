f967599310c6b8c8f0eb31ee2411ff6d0ae76364
evanderbent <evanderbent@bitbucket.org>
2015-03-09 17:57:55 +0100

    Added SUPERIOR ULTRA MAXIMUM WINDOWS createlinks

---
+++ analysis/mklinks.bat
@@ -0,0 +1,6 @@
+del src\GLua\AG\AST.hs
+del src\GLua\AG\PrettyPrint.hs
+del src\GLuanalysis\AG\ControlFlow.hs
+mklink /h src\GLua\AG\AST.hs dist\build\luanalysis\luanalysis-tmp\GLua\AG\AST.hs 
+mklink /h src\GLua\AG\PrettyPrint.hs dist\build\luanalysis\luanalysis-tmp\GLua\AG\PrettyPrint.hs
+mklink /h src\GLuanalysis\AG\ControlFlow.hs dist\build\luanalysis\luanalysis-tmp\GLuanalysis\AG\ControlFlow.hs
