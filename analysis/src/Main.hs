bb3c13b853dd0c5f845aea3e3c8652040b287500
evanderbent <evanderbent@bitbucket.org>
2015-03-09 14:56:25 +0100

    Added function to run the CFG

--- analysis/src/Main.hs
+++ analysis/src/Main.hs
@@ -1,5 +1,42 @@
 module Main where
 
 import GLuanalysis.AG.ControlFlow
+import GLua.Lexer
+import GLua.TokenTypes
+import GLua.Parser
+
+import Data.Char
+
+import System.FilePath
+import System.Environment
+import System.IO
+import System.Exit
+import Control.Monad
+
 
 main = putStrLn "Hello World"
+
+run file =do
+		contents <- readFile file
+
+		-- Lex the file
+		let lex = execParseTokens contents
+		let tokens = fst lex
+		let errors = snd lex
+		
+		unless (null errors) $ do
+			mapM_ print errors
+			-- Attempt to fix errors when asked
+			when (True) $ do
+				writeFile file . concatMap show $ tokens
+				putStrLn "Success"
+				exitSuccess
+
+			exitWith (ExitFailure 1)
+
+		let ast = parseGLua tokens
+		
+		putStrLn . show . getGraph . fst $ ast
+		
+		putStrLn "\nErrors:\n"
+		putStrLn . show . snd $ ast
