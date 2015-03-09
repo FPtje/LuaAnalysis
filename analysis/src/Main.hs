ca3165bc7953f6e63136c1a59de955e0a03ba82b
FPtje <fpeijnenburg@gmail.com>
2015-03-09 18:09:57 +0100

    Pretty print graph

--- analysis/src/Main.hs
+++ analysis/src/Main.hs
@@ -5,6 +5,7 @@ import GLua.Lexer
 import GLua.TokenTypes
 import GLua.Parser
 import GLua.AG.PrettyPrint
+import Data.Graph.Inductive.Graph
 
 import Data.Char
 import Data.List (nub,(\\))
@@ -25,7 +26,7 @@ run file =do
 		let lex = execParseTokens contents
 		let tokens = fst lex
 		let errors = snd lex
-		
+
 		unless (null errors) $ do
 			mapM_ print errors
 			-- Attempt to fix errors when asked
@@ -37,21 +38,21 @@ run file =do
 			exitWith (ExitFailure 1)
 
 		let ast = parseGLua tokens
-		
+
 		putStrLn "\nGraph:\n"
-		putStrLn . show . getGraph . fst $ ast
-		
+		putStrLn . prettify . getGraph . fst $ ast
+
 		putStrLn "\nNodes\n"
 		putStrLn . show . map fst . getNodes. fst $ ast
-		
+
 		putStrLn "\nDuplicate Nodes\n"
 		putStrLn . show $ (map fst $ getNodes. fst $ ast) \\ (nub . map fst . getNodes. fst $ ast)
-		
+
 		putStrLn "\nEdges\n"
 		putStrLn . show . getEdges . fst $ ast
-		
+
 		putStrLn "\nErrors:\n"
 		putStrLn . show . snd $ ast
-		
+
 		putStrLn "Pretty printed code:"
 		putStrLn . show . fst $ ast
