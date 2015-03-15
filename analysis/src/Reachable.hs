abda3bc8cac15af9b1590c0066b7621493d4025f
evanderbent <eduardvdbent@gmail.com>
2015-03-15 17:49:52 +0100

    Made start with embellishment of reachability

--- analysis/src/Reachable.hs
+++ analysis/src/Reachable.hs
@@ -7,8 +7,35 @@ import Data.Graph.Inductive.PatriciaTree
 import GLua.AG.AST
 
 import Data.List (union,(\\))
+import Data.Map
 import MonotoneFramework
 
 -- | Monotone framework instance to find all reachable nodes on a graph from the starting vertex.
-mFramework :: MF Bool
+mFramework :: MF {- Map [Node] -} Bool
 mFramework = MF {joinOp=(||),joinOpReturn=(||),iota=True,bottom=False,consistent= \a b _ -> a && b,transfer= \_ y -> y, transferReturn= \_ _ b c -> c , outfun= \l' _ (gr,_) -> out gr l'}
+
+type EmbellishedReach = Map [Node] Bool
+
+rJoin :: EmbellishedReach -> EmbellishedReach -> EmbellishedReach
+rJoin = unionWith (||)
+
+rJoinR :: EmbellishedReach -> EmbellishedReach -> EmbellishedReach
+rJoinR = undefined
+
+rIota :: EmbellishedReach
+rIota = fromList [([],False)]
+
+rBottom :: EmbellishedReach
+rBottom = fromList [([],True)]
+
+rConsistent :: EmbellishedReach -> EmbellishedReach -> EdgeLabel -> Bool
+rConsistent = \x y z -> and . Prelude.map snd . toList $ unionWith (\a b -> a && b) x y
+
+rTransfer :: NodeThing -> NodeThing ->  EmbellishedReach ->  EmbellishedReach ->  EmbellishedReach
+rTransfer = undefined
+
+rTransferReturn :: NodeThing -> NodeThing ->  EmbellishedReach -> EmbellishedReach ->  EmbellishedReach
+rTransferReturn = undefined
+
+rOutFun ::  Node -> EmbellishedReach -> AnalysisGraph -> [AEdge]
+rOutFun = undefined
