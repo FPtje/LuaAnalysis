999921ef4afdc9c85cee234aba1a29cea6c28eef
evanderbent <eduardvdbent@gmail.com>
2015-03-16 14:55:58 +0100

    Added examples, report

---
+++ examples/example2.lua
@@ -0,0 +1,18 @@
+x = 1
+b = function(f)
+if f then
+a = f
+else 
+a = f
+end
+return a
+end
+x = b(true)
+y = x
+if not x then
+print(y)
+end
+g = true
+while not g do
+print(y)
+end
---
+++ examples/example3.lua
@@ -0,0 +1,9 @@
+x = true
+z = false
+if z then
+x = true
+break
+else
+x = z
+end
+if x then x = true else x = false end
